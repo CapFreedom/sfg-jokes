adf9a0b1217b3a867ff69a5d58c33bcc46035deb
capfreedom <a9122297039@ya.ru>
2019-03-14 14:22:55 +0500

    add joke controller

---
+++ src/main/java/com/example/sfgjokes/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package com.example.sfgjokes.controllers;
+
+import com.example.sfgjokes.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"", "/"})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
